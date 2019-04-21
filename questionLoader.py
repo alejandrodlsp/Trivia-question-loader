f9af4617325e3661b86030c0025a79c8897dd399
alejandrodlsp <alejandrodlsp@hotmail.es>
2019-04-21 12:48:17 +0100

    Update comments

--- questionLoader.py
+++ questionLoader.py
@@ -1,9 +1,17 @@
-from tkinter import messagebox as mb
+###################################################
+#	Alejandro de los Santos Puerto
+#	alejandrodlsp.com
+###################################################
+
 import requests
 import json
 
+# Current session token, 0 if nil
 session_token = 0
 
+
+#
+# requests data from API and returns it as a string
 def request_data(amount, category, difficulty, question_type):
 	if session_token == 0 or session_token == None:
 		get_session()
@@ -13,10 +21,15 @@ def request_data(amount, category, difficulty, question_type):
 	data = response.text
 	return data
 
+
+#
+# requests a session to the opentdb API and saves it
 def get_session():
-	url = "https://opentdb.com/api_token.php?command=request"
+	url = "https://opentdb.com/api_token.php?command=request"	# API token request url
 	response = requests.get(url = url)
 	data = json.loads(response.text)
+
+	# parse response code
 	if data["response_code"] == 0:
 		session_token = data["token"]
 		print("Session token: " + str(session_token) + "\n")
@@ -24,18 +37,26 @@ def get_session():
 		show_error("Session error", "Could not retrieve a session token")
 
 
+#
+# shows an error message
 def show_error(title, error):
-	exit_response = mb.askyesno(title, error + ", exit program?")
-	if exit_response == True:
-		sys.exit()
+	print(title + " : " + error)
+	sys.exit()
 
+
+
+###
+# question set class
 class question_set():
+
+	#
+	# class constructor
 	def __init__(self, amount, category, difficulty, question_type):
 		while True:
-			self.data = request_data(amount, category, difficulty, question_type)
-			self.__dict__ = json.loads(self.data)
+			self.data = request_data(amount, category, difficulty, question_type)	# request data to api
+			self.__dict__ = json.loads(self.data)									# deserialize raw json data into itself
 
-			if self.response_code == 1:
+			if self.response_code == 1:												# parse response code
 				show_error("No Results", "Request could not return results")
 			elif self.response_code == 2:
 				show_error("Invalid Parameter", "Request contains an invalid parameter")
@@ -47,10 +68,16 @@ class question_set():
 				get_session()
 			elif self.response_code == 0:
 				break
-		self.set = self.results
 
+		# save request serialized results
+		self.set = self.Results
+
+
+	#
+	# returns a list of the question objects
 	def get(self):
 		questions = []
+		# loop through all the request result objects and create a question object out of each one
 		for i in range(0,len(self.set)):
 			question_text = self.set[i]["question"]
 			correct_answer = self.set[i]["correct_answer"]
@@ -58,8 +85,14 @@ class question_set():
 			
 			qs = question(question_text, correct_answer, incorrect_answers)
 			questions.append(qs)
+
+		#return the list of question objects
 		return questions
 
+
+
+###
+# question class
 class question():
 	def __init__(self, question, correct_answer, incorrect_answers):
 		self.question = question
